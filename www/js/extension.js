c72957fe11c43a21dab0100f34fe7cbca85fd19d
Zhong Wu <wuzhong@domiler.com>
2016-08-21 11:44:53 +0800

    Update the UI

--- www/js/extension.js
+++ www/js/extension.js
@@ -1,7 +1,9 @@
 AutodeskNamespace('Viewing.ClassroomTrainning');
 
 Viewing.ClassroomTrainning.Extension = function( viewer, option ){
-    Autodesk.Viewing.Extension.call( this, viewer, option );    
+    Autodesk.Viewing.Extension.call( this, viewer, option );  
+    _viewer = viewer;
+    _self = this;
 };
 
 
@@ -11,20 +13,44 @@ Viewing.ClassroomTrainning.Extension.prototype.constructor = Viewing.ClassroomTr
 
 Viewing.ClassroomTrainning.Extension.prototype.onToolbarCreated = function(e){
     console.log('toolbar created event is called');
-    this.viewer.removeEventListener(av.TOOLBAR_CREATED_EVENT, this.onToolbarCreated);
-    this.createUI();
+    _viewer.removeEventListener(Autodesk.Viewing.TOOLBAR_CREATED_EVENT, _self.onToolbarCreated);
+    _self.createMyUI();
 };
 
-Viewing.ClassroomTrainning.Extension.prototype.createUI = function(){
+Viewing.ClassroomTrainning.Extension.prototype.createMyUI = function(){
     console.log('create UI');
+    // Button 1
+    var button1 = new Autodesk.Viewing.UI.Button('my-view-front-button');
+    button1.onClick = function(e) {
+      _viewer.setViewCube('front');
+    };
+//    button1.addClass('my-view-front-button');
+    button1.setToolTip('View front');
+
+    // Button 2
+    var button2 = new Autodesk.Viewing.UI.Button('my-view-back-button');
+    button2.onClick = function(e) {
+      _viewer.setViewCube('back');
+    };
+//    button2.addClass('my-view-back-button');
+    button2.setToolTip('View Back');
+
+    // SubToolbar
+    this.subToolbar = new Autodesk.Viewing.UI.ControlGroup('my-custom-view-toolbar');
+    this.subToolbar.addControl(button1);
+    this.subToolbar.addControl(button2);
+
+    _viewer.toolbar.addControl(this.subToolbar);
+
 };
 
 
 Viewing.ClassroomTrainning.Extension.prototype.load  = ()=>{
-    if( this.viewer.toolbar){
-        this.createUI();
+    if( _viewer.toolbar){
+        _self.createMyUI();
     }else{
-        this.viewer.addEventListener(av.TOOLBAR_CREATED_EVENT, this.onToolbarCreated );
+        _viewer.addEventListener(Autodesk.Viewing.TOOLBAR_CREATED_EVENT, _self.onToolbarCreated );
+        console.log('Events are registered');
     }
 
     console.log('My extension is loaded');
@@ -34,8 +60,8 @@ Viewing.ClassroomTrainning.Extension.prototype.load  = ()=>{
 
 
 Viewing.ClassroomTrainning.Extension.prototype.unload  = ()=>{
-    
-    this.viewer.removeEventListener(av.TOOLBAR_CREATED_EVENT, this.onToolbarCreated );
+    console.log('My extension is unloaded');
+//    this.viewer.removeEventListener(Autodesk.Viewing.TOOLBAR_CREATED_EVENT, _self.onToolbarCreated );
     return true;
     
 };
