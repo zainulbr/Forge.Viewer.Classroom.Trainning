7ec4e314a35532b5a05acfe29e2a4b22fffb1f49
Zhong Wu <wuzhong@domiler.com>
2016-08-23 15:45:22 +0800

    Sphere create

--- www/js/extension.js
+++ www/js/extension.js
@@ -17,8 +17,41 @@ Viewing.ClassroomTrainning.Extension.prototype.onToolbarCreated = function(e){
     _self.createMyUI();
 };
 
-Viewing.ClassroomTrainning.Extension.prototype.createMyUI = function(){
-    console.log('create UI');
+
+Viewing.ClassroomTrainning.Extension.prototype.createToggler = function (button, click, unclick) {
+    return function () {
+        var state = button.getState();
+        if (state === Autodesk.Viewing.UI.Button.State.INACTIVE) {
+            button.setState(Autodesk.Viewing.UI.Button.State.ACTIVE);
+            click();
+        } else if (state === Autodesk.Viewing.UI.Button.State.ACTIVE) {
+            button.setState(Autodesk.Viewing.UI.Button.State.INACTIVE);
+            unclick();
+        }
+    };
+};
+
+
+Viewing.ClassroomTrainning.Extension.prototype.screenPointToHitPoint = function( screenPoint ){
+    var viewport = _viewer.navigation.getScreenViewport();
+    var n = {
+        x: ( screenPoint.x - viewport.left)/viewport.width,
+        y: ( screenPoint.y - viewport.top)/viewport.height
+    };
+
+    var hitPoint = _viewer.utilities.getHitPoint( n.x, n.y );
+    return hitPoint;
+};
+
+
+Viewing.ClassroomTrainning.Extension.prototype.drawModel = function( pos ){
+
+};
+
+
+
+
+Viewing.ClassroomTrainning.Extension.prototype.createSimpleToolbar = function(){
     // Button 1
     var button1 = new Autodesk.Viewing.UI.Button('my-view-front-button');
     button1.icon.style.backgroundImage = 'url(../img/frontview.png)';
@@ -45,6 +78,74 @@ Viewing.ClassroomTrainning.Extension.prototype.createMyUI = function(){
 
     _viewer.toolbar.addControl(this.subToolbar);
     
+};
+
+Viewing.ClassroomTrainning.Extension.prototype.createDivToolbar = function(){
+    
+
+    var createToggleButton = function(){
+        
+        var geoMaterial = null;
+        
+        var initMaterial = function(){
+            if( geoMaterial == null ){
+                geoMaterial = new THREE.MeshPhongMaterial({color: 0xff0000, specular: 0x009900, shininess: 30, 
+                                                         shading: THREE.FlatShading });
+                _viewer.impl.matman().addMaterial( 'material-red', geoMaterial, true );
+                console.log('Red material is added');                
+            }
+        };
+        
+        var uninitMaterial = function(){
+            if( geoMaterial ){
+                _viewer.impl.matman().removeMaterial('material-red');
+                geoMaterial = null;          
+                console.log('Red material is removed');                
+            };
+            
+        };
+
+        var createGeometry = function(event){
+            var screenPoint = {
+                x: event.clientX, 
+                y: event.clientY
+            };
+            
+            var worldPoint = _self.screenPointToHitPoint(screenPoint);
+            if( worldPoint ){
+                console.log('find a point by click.'+ worldPoint);
+                // create a sphere    
+                var sphere = new THREE.SphereGeometry( 1, 32 );
+                var mesh = new THREE.Mesh(sphere, geoMaterial);
+                mesh.position.set(worldPoint.x, worldPoint.y, worldPoint.z);
+                _viewer.impl.scene.add(mesh);
+                console.log('created a mesh and added to the scene.' + mesh.name);
+                _viewer.impl.sceneUpdated(true);
+            }
+            else
+                console.log('can not find a point by click');
+        };
+
+        var toggleButtonOn = function(){
+            $('#viewer').bind('click', createGeometry );
+
+        };
+
+        var toggleButtonOff = function(){
+            $('#viewer').unbind('click', createGeometry );
+        };
+
+
+        var toggleButton = new Autodesk.Viewing.UI.Button('my-toggle-button');
+        toggleButton.icon.style.backgroundImage = 'url(../img/button.png)';
+        toggleButton.setToolTip('Create a sphere');
+        toggleButton.addClass('my-toggle-button');
+        toggleButton.onClick = _self.createToggler(toggleButton, toggleButtonOn, toggleButtonOff );
+ 
+        return toggleButton;
+        
+    };
+
     // Add a seperated button
     var toolbarDivHtml = "<div id='divToolbar'></div>";
     $(_viewer.container).append(toolbarDivHtml);
@@ -59,18 +160,26 @@ Viewing.ClassroomTrainning.Extension.prototype.createMyUI = function(){
     var ctrGroup = new Autodesk.Viewing.UI.ControlGroup('my-seperated-toolbar');
     var button3  = new Autodesk.Viewing.UI.Button('my-seperated-button');
     button3.icon.style.backgroundImage = 'url(../img/button.png)';
-    button3.setToolTip('Add a sphere');
+    button3.setToolTip('I am a seperated button');
     button3.addClass('my-seperated-button');
-    button3.onClick = function(e){
-        console.log('I will add a sphere');
+    button3.onClick = function(){
+        console.log('I am a div button');
+        alert('I am a div button');
     };
+    
+    
     ctrGroup.addControl(button3);
+    var toggleButton = createToggleButton();
+    ctrGroup.addControl(toggleButton);
     
     $('#divToolbar')[0].appendChild(ctrGroup.container);
-    
-    
-    
+};
 
+
+Viewing.ClassroomTrainning.Extension.prototype.createMyUI = function(){
+    console.log('create UI');
+    _self.createSimpleToolbar();
+    _self.createDivToolbar();
 };
 
 
